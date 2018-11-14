c880a2f9e6a1569903b68b28758ebb9391f53dec
vincenttanujaya <vincent.tanujaya@gmail.com>
2018-11-14 12:57:55 +0700

    Edit Tampilan Admin

--- resources/views/layouts/appadmin.blade.php
+++ resources/views/layouts/appadmin.blade.php
@@ -19,161 +19,105 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-<link rel="stylesheet" href="css/lib/lobipanel/lobipanel.min.css">
-<link rel="stylesheet" href="css/separate/vendor/lobipanel.min.css">
-<link rel="stylesheet" href="css/lib/jqueryui/jquery-ui.min.css">
-<link rel="stylesheet" href="css/separate/pages/widgets.min.css">
-<link rel="stylesheet" href="css/separate/pages/login.min.css">
-    <link rel="stylesheet" href="css/lib/font-awesome/font-awesome.min.css">
-    <link rel="stylesheet" href="css/lib/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
-</head>
-<body class="with-side-menu control-panel control-panel-compact">
-    <div id="app">
-            <header class="site-header">
-                    <div class="container-fluid">
-                        <a href="#" class="site-logo">
-                            <img class="hidden-md-down" src="img/logoits.jpg" alt="">
-                            <img class="hidden-lg-down" src="img/logoits.jpg" alt="">
-                        </a>
-                        <button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
-                            <span>toggle menu</span>
-                        </button>
-                
-                        <button class="hamburger hamburger--htla">
-                            <span>toggle menu</span>
-                        </button>
-                        <div class="site-header-content">
-                            <div class="site-header-content-in">
-                                <div class="site-header-shown">
-                
-                                
-                
-                                    <div class="dropdown user-menu">
-                                        <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <img src="img/avatar-2-64.png" alt="">
-                                        </button>
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
-                                            <a class="dropdown-item" onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();"><span class="font-icon glyphicon glyphicon-log-out"></span>Logout</a>
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                @csrf
-                                            </form>
-                                        </div>
-                                    </div>
-                
-                                    <button type="button" class="burger-right">
-                                        <i class="font-icon-menu-addl"></i>
-                                    </button>
-                                </div><!--.site-header-shown-->
-
-                                    </div><!--.site-header-collapsed-in-->
-                                </div><!--.site-header-collapsed-->
-                            </div><!--site-header-content-in-->
-                        </div><!--.site-header-content-->
-                    </div><!--.container-fluid-->
-                </header><!--.site-header-->
-                <nav class="side-menu">
-                        <ul class="side-menu-list">
-                            <li class="red">
-                                <a href="#">
-                                    <i class="font-icon font-icon-notebook"></i>
-                                    <span class="lbl">Permohonan</span>
-                                </a>
-                            </li>
-                            <li class="green">
-                                <a href="#">
-                                    <i class="font-icon font-icon-zigzag"></i>
-                                    <span class="lbl">Riwayat</span>
-                                </a>
-                            </li>
-                            <li class="blue">
-                                <a href="#">
-                                    <i class="font-icon font-icon-doc"></i>
-                                    <span class="lbl">Template</span>
-                                </a>
-                            </li>
-                        </ul>
-                    
-                        
-                    </nav><!--.side-menu-->
-        {{-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <img src="img/logoits.jpg" height="50">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    E-Surat Informatika
+    <link rel="stylesheet" href="css/lib/lobipanel/lobipanel.min.css">
+    <link rel="stylesheet" href="css/separate/vendor/lobipanel.min.css">
+    <link rel="stylesheet" href="css/lib/jqueryui/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/separate/pages/widgets.min.css">
+        <link rel="stylesheet" href="css/lib/font-awesome/font-awesome.min.css">
+        <link rel="stylesheet" href="css/lib/bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body class="with-side-menu theme-rebecca-purple">
+    
+        <header class="site-header">
+            <div class="container-fluid">
+                <a href="#" class="site-logo">
+                    <img class="hidden-md-down" src="img/logoITSPutih.png" alt="">
+                    <img class="hidden-lg-down" src="img/IF.gif" alt="">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <button class="hamburger hamburger--htla">
+                    <span>toggle menu</span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                {{-- <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown"> --}}
-                                    {{-- <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                <div class="site-header-content">
+                    <div class="site-header-content-in text-center">
+                        <div class="site-header-shown">
+                            
+                            <div class="dropdown user-menu">
+                                <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <img src="img/avatar-2-64.png" alt="">
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
+                                    <a class="dropdown-item"><span class="font-icon glyphicon glyphicon-user"></span>{{ Auth::user()->name }}</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();"><span class="font-icon glyphicon glyphicon-log-out"></span>Logout</a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-                                {{-- </div> --}}
-                            {{-- </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
+                                </div>
+                            </div>
+                        </div><!--.site-header-shown-->
+                        <h5 class="text-center" style="color:white">E-SURAT INFORMATIKA ITS</h5>
+                    </div><!--site-header-content-in-->
+                </div><!--.site-header-content-->
+            </div><!--.container-fluid-->
+        </header><!--.site-header-->
+    
+        <div class="mobile-menu-left-overlay"></div>
+        <nav class="side-menu">
+            <ul class="side-menu-list">
+                <li class="grey">
+                    <a href="#">
+                        <i class="font-icon font-icon-notebook"></i>
+                        <span class="lbl">Permohonan</span>
+                    </a>
+                </li>
+                <li class="grey">
+                    <a href="#">
+                        <i class="font-icon font-icon-zigzag"></i>
+                        <span class="lbl">Riwayat</span>
+                    </a>
+                </li>
+                <li class="grey">
+                    <a href="#">
+                        <i class="font-icon font-icon-doc"></i>
+                        <span class="lbl">Template</span>
+                    </a>
+                </li>
+                <li class="grey">
+                    <a href="#">
+                        <i class="font-icon font-icon-user"></i>
+                        <span class="lbl">Administrator</span>
+                    </a>
+                </li>
+            </ul>
+        </nav><!--.side-menu-->
+    
         <div class="page-content">
                 <div class="container-fluid">           
                     @yield('content')
                 </div>
-        </div>
-    </div>
-    <script src="js/lib/jquery/jquery-3.2.1.min.js"></script>
-	<script src="js/lib/popper/popper.min.js"></script>
-	<script src="js/lib/tether/tether.min.js"></script>
-	<script src="js/lib/bootstrap/bootstrap.min.js"></script>
-	<script src="js/plugins.js"></script>
-
-	<script type="text/javascript" src="js/lib/jqueryui/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="js/lib/lobipanel/lobipanel.min.js"></script>
-	<script type="text/javascript" src="js/lib/match-height/jquery.matchHeight.min.js"></script>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        </div><!--.page-content-->
     
-    <script>
+        <script src="js/lib/jquery/jquery-3.2.1.min.js"></script>
+        <script src="js/lib/popper/popper.min.js"></script>
+        <script src="js/lib/tether/tether.min.js"></script>
+        <script src="js/lib/bootstrap/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+    
+        <script type="text/javascript" src="js/lib/jqueryui/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/lib/lobipanel/lobipanel.min.js"></script>
+        <script type="text/javascript" src="js/lib/match-height/jquery.matchHeight.min.js"></script>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script>
             $(document).ready(function() {
-                $('.panel').each(function () {
-                    try {
-                        $(this).lobiPanel({
-                            sortable: true
-                        }).on('dragged.lobiPanel', function(ev, lobiPanel){
-                            $('.dahsboard-column').matchHeight();
-                        });
-                    } catch (err) {}
-                });
+                try {
+                    $('.panel').lobiPanel({
+                        sortable: true
+                    }).on('dragged.lobiPanel', function(ev, lobiPanel){
+                        $('.dahsboard-column').matchHeight();
+                    });
+                } catch (err) {}
     
                 google.charts.load('current', {'packages':['corechart']});
                 google.charts.setOnLoadCallback(drawChart);
@@ -226,7 +170,7 @@
                             gridlines: {
                                 color: '#1ba0fc',
                                 count: 15
-                            }
+                            },
                         },
                         lineWidth: 2,
                         colors: ['#fff'],
@@ -262,7 +206,7 @@
                 });
             });
         </script>
-        
-    <script src="js/app.js"></script> 
-</body>
-</html>
+    
+    <script src="js/app.js"></script>
+    </body>
+    </html>
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,3 @@ Route::get('/kerjapraktik', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
