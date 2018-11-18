486f2723d5248f5da429af12af22c1ae97efe6da
dewisekar <sekar.6499@gmail.com>
2018-11-18 17:39:47 +0700

    dynamic form udah jadi ciyaaa

--- resources/views/user/kerjapraktik.blade.php
+++ resources/views/user/kerjapraktik.blade.php
@@ -18,6 +18,7 @@
 		<div class="row">
 			<div class="col-md-12">
 				<form>
+					
 					<div class="form-group row">
 						<label for="inputPassword" class="col-sm-2 form-control-label">Nama Pengaju</label>
 						<div class="col-sm-10">
@@ -58,51 +59,60 @@
 			</div>
 		</div>				
 	</div>  
-	<div class="box-typical box-typical-padding">      
-		<div class="row">
+	<div class="box-typical box-typical-padding" data-role="dynamic-fields">      
+		<div class="row ">
 			<div class="col-lg-4">
-				<div data-role="dynamic-fields">
-					<div class="form-inline">					
-						<div class="form-group input-group">
-							<input type="text" class="form-control" aria-label="Text input with 	dropdown button">
-							<div class="input-group-append">
-								<button type="button" class="btn btn-success"  data-role="add">	Add
-								</button>
-								<button type="button" class="btn btn-danger"  data-role="remove"	>Remove
-								</button>
-							</div>
-						</div>			
-					</div>
-				</div>
+				<fieldset class="form-group">
+					<label class="form-label semibold" for="exampleInput">First Name </label> 
+					<input type="text" class="form-control" id="exampleInput" placeholder="First Name">
+				</fieldset>
 			</div>
-			
+			<div class="col-lg-4">
+				<fieldset class="form-group">
+					<label class="form-label" for="exampleInputEmail1">Email address</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" value="mail@mail.com">
+				</fieldset>
+			</div>
+			<div class="col-lg-4">
+				<fieldset class="form-group">
+					<label class="form-label" for="exampleInputEmail1">Tambah/Hapus Mahasiswa</label>
+					<button type="button" class="btn btn-success"  data-role="add">	Add
+							</button>
+					<button type="button" class="btn btn-danger" id="removebutton"  data-role="remove"	>Remove
+							</button>
+				</fieldset>
+			</div>	
 		</div><!--.row-->		
 	</div>          
 @endsection
 @section('script')
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script type="text/javascript">
-	var counter = 1;	
+	var counter =1;
 	$(document).on(
 		'click',
-		'[data-role="dynamic-fields"] > .form-inline [data-role="remove"]',
+		'[data-role="dynamic-fields"] > .row [data-role="remove"]',
 		function(e){
-			e.preventDefault();
-			$(this).closest('.form-inline').remove();
+			if (counter>1){
+				e.preventDefault();
+				$(this).closest('.row').remove();
+				counter--;
+			}		
 		}
 	);
 	$(document).on(
 		'click',
-		'[data-role="dynamic-fields"] > .form-inline [data-role="add"]',
+		'[data-role="dynamic-fields"] > .row [data-role="add"]',
 		function(e){
 			e.preventDefault();
 			var container = $(this).closest('[data-role="dynamic-fields"]');
-			new_field_group = container.children().filter('.form-inline:first-child').clone();
+			new_field_group = container.children().filter('.row:first-child').clone();
 			new_field_group.find('input').each(function(){
 				$(this).val('');
+
 			});
-			container.append(new_field_group);
-			
+			counter++;	
+			container.append(new_field_group);		
 		}
 	);
 </script>
