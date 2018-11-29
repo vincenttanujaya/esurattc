d1df558bdc2c20b272eaebc519a9e43791a7e903
dewisekar <sekar.6499@gmail.com>
2018-11-29 22:58:09 +0700

    update dikit
    kalo mau liat mengelola lalalala di blade pejabat aja dah jadi

--- resources/views/admin/pejabat.blade.php
+++ resources/views/admin/pejabat.blade.php
@@ -65,8 +65,7 @@
 					    </tr>
 					</tfoot>
 					<tbody>
-                        @foreach($pejabat as $item)    
-                                         
+                        @foreach($pejabat as $item)                                             
 					        <tr>
                                 <td>{{$counter++}}.</td>
 					        	<td>{{$item->nama_pejabat}}</td>
@@ -137,19 +136,18 @@
                                                 <div class="modal-body">
                                                     <p> Apakah anda yakin ingin menghapus data? </p>
                                                 </div>
-                                                    <form action="{{url('/deletepejabat/'.$item->id_pejabat)}}" method="POST")> 
-                                                        @csrf                                           
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                            <button type="submit" class="btn btn-danger">Hapus</button>
-                                                        </div> 
-                                                    </form>   
+                                                <form action="{{url('/deletepejabat/'.$item->id_pejabat)}}" method="POST")> 
+                                                    @csrf                                           
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                        <button type="submit" class="btn btn-danger">Hapus</button>
+                                                    </div> 
+                                                </form>   
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            
+                            </div>                            
                         @endforeach				
 					</tbody>
 				</table>			
@@ -157,34 +155,7 @@
 		</div>
 	</div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="editsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title text-center" id="exampleModalLongTitle">Konfirmasi Penghapusan</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                  Apakah anda yakin untuk melakukan penghapusan data?
-                  <p id="demo"></p>
-                </div>
-                <form action="{{url('/deletepejabat2/')}}" method="GET")>
-                    @csrf
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Jadi</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
-
 @section('dtable')
     <script>
         $(function() {
